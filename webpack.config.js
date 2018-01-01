26e758521f0759c614079ae6484131f8736b8f39
Jim Merioles <jimwisleymerioles@gmail.com>
2018-01-01 20:01:22 +0800

    Added production build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 let path = require('path');
+let UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+let isProd = process.env.NODE_ENV === 'production';
+let filename = isProd? 'app.min.js' : 'app.js';
+let plugins = [];
+
+if(isProd) {
+    plugins.push(new UglifyJsPlugin());
+}
 
 module.exports = {
   entry: './src/js/app.js',
   output: {
-    filename: 'app.js',
+    filename: filename,
     path: path.resolve(__dirname, 'dist/js')
-  }
+  },
+  plugins: plugins
 };
